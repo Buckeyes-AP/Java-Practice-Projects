eadd305c60f78b008c6680b61025a9657e38c635
Alex Poirier <alexmatthewpoirier@gmail.com>
2023-10-14 14:04:33 -0400

    need to format print statements

--- InterestCalculator/src/main/java/org/example/InterestCalculator.java
+++ InterestCalculator/src/main/java/org/example/InterestCalculator.java
@@ -4,19 +4,40 @@ package org.example;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class InterestCalculator {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String choice = "y";
 
-        double value = 4944.5;
-        String formattedNumber = String.format("%,.2f", value);
-        System.out.println("$" + formattedNumber);
+        System.out.println("Welcome to the Interest Calculator\n");
+
+        while(choice.equalsIgnoreCase("y")) {
+            System.out.print("Enter loan amount: ");
+            double value = sc.nextDouble();;
+            System.out.print("Enter interest rate: ");
+            double rate = sc.nextDouble();
+
+            double interest = value * rate;
+
+            String formattedNumber = String.format("%,.2f", value);
+            System.out.println("$" + formattedNumber);
+
+            BigDecimal number = BigDecimal.valueOf(rate);
+            BigDecimal percentValue = number.movePointRight(2);
+            System.out.println(percentValue + "%" + "");
+
+            formattedNumber = String.format("%,.2f", interest);
+            System.out.println("$" + formattedNumber);
+
+            System.out.print("Continue? (y/n): ");
+            choice = sc.next();
+            System.out.println();
+        }
+        System.out.println("Goodbye!");
 
 
-        double rate = .01;
-        BigDecimal number = BigDecimal.valueOf(rate);
-        BigDecimal percentValue = number.movePointRight(2);
-        System.out.println(percentValue);
 
 
 
