171026455b52654a3bbe03e94ea82d57bcbe027c
Alex Poirier <alexmatthewpoirier@gmail.com>
2023-10-18 12:27:17 -0400

    completed chapter04 project04

--- CommonDivisorCalculator/src/main/java/org/example/Main.java
+++ CommonDivisorCalculator/src/main/java/org/example/Main.java
@@ -7,26 +7,45 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String choice = "y";
-        System.out.println("Greatest Common Divisor Finder");
+        System.out.println("Greatest Common Divisor Finder \n");
 
-        System.out.print("Enter first number: ");
-        int first = sc.nextInt(); // 12
-        System.out.print("Enter second number: ");
-        int second = sc.nextInt(); // 8
+        
 
-        for (int i = second; i < first; i++) {
 
-            for ( i = second; i < first; i++) {
-                first -= second;
-                System.out.println(first);
+        while (choice.equalsIgnoreCase("y")) {
+
+            System.out.print("Enter first number: ");
+            int x = sc.nextInt();
+            System.out.print("Enter second number: ");
+            int y = sc.nextInt();
+
+            int divisor = 0;
+
+            while (x != 0) {
+
+                for (int i = x; i > 0; i--) {
+                    if (x == 0) {
+                        break;
+                    }
+                    x -= y;
+                    if (x == 0) {
+                        break;
+                    }
+                    if (x < y) {
+                        divisor = x;
+                        x = y;
+                        y = divisor;
+                        continue;
+                    }
+                    x -= divisor;
+                }
+
             }
+            System.out.println("Greatest common divisor: " + y + "\n");
+            System.out.print("Continue? (y/n): ");
+            choice = sc.next();
+            System.out.println();
         }
-
-
-
-
-
-
-
+        System.out.println("Goodbye!");
     }
 }
