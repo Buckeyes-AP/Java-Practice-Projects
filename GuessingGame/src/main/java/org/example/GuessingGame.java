06ac99fbe9bb6f7c2bd79ea3e3c28a70367ca877
Alex Poirier <alexmatthewpoirier@gmail.com>
2023-10-22 13:35:09 -0400

    finished project05 guessing game

--- GuessingGame/src/main/java/org/example/GuessingGame.java
+++ GuessingGame/src/main/java/org/example/GuessingGame.java
@@ -1,10 +1,11 @@
 package org.example;
 
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
-import static org.example.Console.displayWelcome;
-import static org.example.Console.getRandomInt;
+import static org.example.Console.*;
 
 public class GuessingGame {
     public static void main(String[] args) {
@@ -18,7 +19,7 @@ public class GuessingGame {
         String choice  = "y";
 
         while (choice.equalsIgnoreCase("y")) {
-            System.out.println("Enter number: ");
+            System.out.print("Enter number: ");
             int guess = sc.nextInt();
 
             if (guess < 1 || guess > LIMIT) {
@@ -27,12 +28,38 @@ public class GuessingGame {
             }
 
             if (guess < number) {
-                System.out.println("Too low.");
+                System.out.println("Too low! Guess again.\n");
             } else if (guess > number) {
-                System.out.println("Too high.");
+                System.out.println("Too high! Guess again.\n");
+            } else if (count > 7 && guess == number) {
+                System.out.println("What took you so long? Maybe you should take some lessons.");
+                System.out.println("You guessed it in " + count + " tries.\n");
+                choice = getString("Continue? (y/n): ", "y", "n");
+                if (choice.equals("y")) {
+                    count = 1;
+                }
+                System.out.println();
+            }
+                else if (count > 3 && count <= 7 && guess == number) {
+                System.out.println("Not too bad! You've got some potential.");
+                System.out.println("You guessed it in " + count + " tries.\n");
+                choice = getString("Continue? (y/n): ", "y", "n");
+                if (choice.equals("y")) {
+                    count = 1;
+                }
+                System.out.println();
+            } else if (count <= 3 && guess == number) {
+                System.out.println("Great work! You are a mathematical wizard.");
+                System.out.println("You guessed it in " + count + " tries.\n");
+                choice = getString("Continue? (y/n): ", "y", "n");
+                if (choice.equals("y")) {
+                    count = 1;
+                }
+                System.out.println();
             } else {
                 System.out.println("You guessed it in " + count + " tries.\n");
-                break;
+                System.out.print("Continue? (y/n): ");
+                choice = sc.next();
             }
             count++;
         }
