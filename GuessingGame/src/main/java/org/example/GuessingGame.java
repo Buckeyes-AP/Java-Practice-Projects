30a8f2faaf293e58a110973b47c65d23748353ee
Alex Poirier <alexmatthewpoirier@gmail.com>
2023-10-24 11:46:04 -0400

    fixed logic for number of guesses

--- GuessingGame/src/main/java/org/example/GuessingGame.java
+++ GuessingGame/src/main/java/org/example/GuessingGame.java
@@ -9,16 +9,58 @@ import static org.example.Console.*;
 
 public class GuessingGame {
     public static void main(String[] args) {
-        final int LIMIT = 100;
-
-        displayWelcome(LIMIT);
-        int number = getRandomInt(LIMIT);
 
         Scanner sc = new Scanner(System.in);
-        int count = 1;
-        String choice  = "y";
+
+        System.out.println("Welcome to the guess number game!");
+        System.out.println("+++++++++++++++++++++++++++++++++");
+        System.out.println();
+        System.out.println("I'm thinking of a number from 1 to 100 .");
+        System.out.println("Try to guess it.");
+        System.out.println();
+        
+
+        String choice = "y";
 
         while (choice.equalsIgnoreCase("y")) {
+            int count = 0;
+            int guess = 0;
+            int theNumber = (int) (Math.random() * 100) + 1;
+
+            System.out.println("Hint: # is " + theNumber);
+
+            while (guess != theNumber) {
+                guess = Console.getInt("Enter number: ", 0, 101);
+                count++;
+
+                int diff = theNumber - guess;
+                if (diff < -10) {
+                    System.out.println("Way too high!");
+                } else if (diff > 10) {
+                System.out.println("Way too low!");
+                } else if (diff < 0) {
+                    System.out.println("Too high!");
+                } else if (diff > 0) {
+                    System.out.println("Too low!");
+                } else {
+                    System.out.println("You got it in " + count + " tries.");
+
+                    if (count <= 3) {
+                        System.out.println("Great work! You're a mathematical wizard!");
+                    } else if (count <= 7) {
+                        System.out.println("Not too bad. You've got some potential.");
+                    } else {
+                        System.out.println("What took you so long? Maybe you should take some lessons.");
+                    }
+                }
+
+                choice = Console.getString("Try again(y/n)?", "y", "n");
+            }
+
+        /* int count = 1;
+
+        while (choice.equalsIgnoreCase("y")) {
+            int number = getRandomInt(LIMIT);
             System.out.print("Enter number: ");
             int guess = sc.nextInt();
 
@@ -31,6 +73,7 @@ public class GuessingGame {
                 System.out.println("Too low! Guess again.\n");
             } else if (guess > number) {
                 System.out.println("Too high! Guess again.\n");
+
             } else if (count > 7 && guess == number) {
                 System.out.println("What took you so long? Maybe you should take some lessons.");
                 System.out.println("You guessed it in " + count + " tries.\n");
@@ -39,8 +82,8 @@ public class GuessingGame {
                     count = 1;
                 }
                 System.out.println();
-            }
-                else if (count > 3 && count <= 7 && guess == number) {
+
+            } else if (count > 3 && count <= 7 && guess == number) {
                 System.out.println("Not too bad! You've got some potential.");
                 System.out.println("You guessed it in " + count + " tries.\n");
                 choice = getString("Continue? (y/n): ", "y", "n");
@@ -48,6 +91,7 @@ public class GuessingGame {
                     count = 1;
                 }
                 System.out.println();
+
             } else if (count <= 3 && guess == number) {
                 System.out.println("Great work! You are a mathematical wizard.");
                 System.out.println("You guessed it in " + count + " tries.\n");
@@ -56,6 +100,7 @@ public class GuessingGame {
                     count = 1;
                 }
                 System.out.println();
+
             } else {
                 System.out.println("You guessed it in " + count + " tries.\n");
                 System.out.print("Continue? (y/n): ");
@@ -64,5 +109,7 @@ public class GuessingGame {
             count++;
         }
         System.out.println("Goodbye!");
+     */
+        }
     }
 }
