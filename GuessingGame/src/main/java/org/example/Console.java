e7c1743262c84b811281bba145fab4a8414189a9
Alex Poirier <alexmatthewpoirier@gmail.com>
2023-10-25 12:48:21 -0400

    formatted print statements

--- GuessingGame/src/main/java/org/example/Console.java
+++ GuessingGame/src/main/java/org/example/Console.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Console {
     private static Scanner sc = new Scanner(System.in);
 
-    public static void displayWelcome(int limit) {
+    public static void displayWelcome() {
         System.out.println("Welcome to the guess number game!");
         System.out.println("+++++++++++++++++++++++++++++++++");
         System.out.println();
-        System.out.println("I'm thinking of a number from 1 to " + limit + ".");
+        System.out.println("I'm thinking of a number from 1 to 100.");
         System.out.println("Try to guess it.");
         System.out.println();
     }
@@ -26,8 +26,8 @@ public class Console {
         while (!isValid) {
             System.out.print(prompt);
             s = sc.nextLine();
-            if (s.equals("")) {
-                System.out.println("Error! This entry is required. Try again.");
+            if (s.isEmpty()) {
+                System.out.println("Error! This entry is required. Try again.\n");
             } else {
                 isValid = true;
             }
@@ -42,7 +42,7 @@ public class Console {
             s = getString(prompt);
             if (!s.equalsIgnoreCase(s1) && !s.equalsIgnoreCase(s2)) {
                 System.out.println("Error! Entry must be '" + s1 + "' or '" +
-                        s2 + "'. Try again.");
+                        s2 + "'. Try again.\n");
             } else {
                 isValid = true;
             }
@@ -59,7 +59,7 @@ public class Console {
                 i = sc.nextInt();
                 isValid = true;
             } else {
-                System.out.println("Error! Invalid integer. Try again.");
+                System.out.println("Error! Invalid integer. Try again.\n");
             }
             sc.nextLine(); // discard any other data entered on the line
         }
@@ -73,10 +73,10 @@ public class Console {
             i = getInt(prompt);
             if (i <= min) {
                 System.out.println(
-                        "Error! Number must be greater than " + min + ".");
+                        "Error! Number must be greater than " + min + ".\n");
             } else if (i >= max) {
                 System.out.println(
-                        "Error! Number must be less than " + max + ".");
+                        "Error! Number must be less than " + max + ".\n");
             } else {
                 isValid = true;
             }
@@ -93,7 +93,7 @@ public class Console {
                 d = sc.nextDouble();
                 isValid = true;
             } else {
-                System.out.println("Error! Invalid number. Try again.");
+                System.out.println("Error! Invalid number. Try again.\n");
             }
             sc.nextLine(); // discard any other data entered on the line
         }
@@ -107,10 +107,10 @@ public class Console {
             d = getDouble(prompt);
             if (d <= min) {
                 System.out.println(
-                        "Error! Number must be greater than " + min + ".");
+                        "Error! Number must be greater than " + min + ".\n");
             } else if (d >= max) {
                 System.out.println(
-                        "Error! Number must be less than " + max + ".");
+                        "Error! Number must be less than " + max + ".\n");
             } else {
                 isValid = true;
             }
--- GuessingGame/src/main/java/org/example/GuessingGame.java
+++ GuessingGame/src/main/java/org/example/GuessingGame.java
@@ -1,24 +1,16 @@
 package org.example;
 
 
-import org.w3c.dom.ls.LSOutput;
-
-import java.util.Scanner;
 
 import static org.example.Console.*;
 
 public class GuessingGame {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Welcome to the guess number game!");
-        System.out.println("+++++++++++++++++++++++++++++++++");
-        System.out.println();
-        System.out.println("I'm thinking of a number from 1 to 100 .");
-        System.out.println("Try to guess it.");
-        System.out.println();
-        
+
+        displayWelcome();
+
 
         String choice = "y";
 
@@ -35,81 +27,30 @@ public class GuessingGame {
 
                 int diff = theNumber - guess;
                 if (diff < -10) {
-                    System.out.println("Way too high!");
+                    System.out.println("Way too high!\n");
                 } else if (diff > 10) {
-                System.out.println("Way too low!");
+                    System.out.println("Way too low!\n");
                 } else if (diff < 0) {
-                    System.out.println("Too high!");
+                    System.out.println("Too high!\n");
                 } else if (diff > 0) {
-                    System.out.println("Too low!");
+                    System.out.println("Too low!\n");
                 } else {
-                    System.out.println("You got it in " + count + " tries.");
+                    System.out.println("You got it in " + count + " tries.\n");
 
                     if (count <= 3) {
-                        System.out.println("Great work! You're a mathematical wizard!");
+                        System.out.println("Great work! You're a mathematical wizard!\n");
                     } else if (count <= 7) {
-                        System.out.println("Not too bad. You've got some potential.");
+                        System.out.println("Not too bad. You've got some potential.\n");
                     } else {
-                        System.out.println("What took you so long? Maybe you should take some lessons.");
+                        System.out.println("What took you so long? Maybe you should take some lessons.\n");
                     }
                 }
 
-                choice = Console.getString("Try again(y/n)?", "y", "n");
+
             }
-
-        /* int count = 1;
-
-        while (choice.equalsIgnoreCase("y")) {
-            int number = getRandomInt(LIMIT);
-            System.out.print("Enter number: ");
-            int guess = sc.nextInt();
-
-            if (guess < 1 || guess > LIMIT) {
-                System.out.println("Invalid guess. Try again.");
-                continue;
-            }
-
-            if (guess < number) {
-                System.out.println("Too low! Guess again.\n");
-            } else if (guess > number) {
-                System.out.println("Too high! Guess again.\n");
-
-            } else if (count > 7 && guess == number) {
-                System.out.println("What took you so long? Maybe you should take some lessons.");
-                System.out.println("You guessed it in " + count + " tries.\n");
-                choice = getString("Continue? (y/n): ", "y", "n");
-                if (choice.equals("y")) {
-                    count = 1;
-                }
-                System.out.println();
-
-            } else if (count > 3 && count <= 7 && guess == number) {
-                System.out.println("Not too bad! You've got some potential.");
-                System.out.println("You guessed it in " + count + " tries.\n");
-                choice = getString("Continue? (y/n): ", "y", "n");
-                if (choice.equals("y")) {
-                    count = 1;
-                }
-                System.out.println();
-
-            } else if (count <= 3 && guess == number) {
-                System.out.println("Great work! You are a mathematical wizard.");
-                System.out.println("You guessed it in " + count + " tries.\n");
-                choice = getString("Continue? (y/n): ", "y", "n");
-                if (choice.equals("y")) {
-                    count = 1;
-                }
-                System.out.println();
-
-            } else {
-                System.out.println("You guessed it in " + count + " tries.\n");
-                System.out.print("Continue? (y/n): ");
-                choice = sc.next();
-            }
-            count++;
+            choice = Console.getString("Try again(y/n)?: ", "y", "n");
+            System.out.println();
         }
-        System.out.println("Goodbye!");
-     */
-        }
+
     }
 }
