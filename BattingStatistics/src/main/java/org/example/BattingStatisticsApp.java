13f529f51816c72e2bfd05b79cf55f336b1e3536
Alex Poirier <alexmatthewpoirier@gmail.com>
2023-10-31 12:42:23 -0400

    completed project8-1

--- BattingStatistics/src/main/java/org/example/BattingStatisticsApp.java
+++ BattingStatistics/src/main/java/org/example/BattingStatisticsApp.java
@@ -1,46 +1,51 @@
 package org.example;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class BattingStatisticsApp {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Welcome to the Batting Average Calculator");
+        Console.displayWelcome();
+        String choice = "y";
+        while (choice.equalsIgnoreCase("y")) {
 
+            int times = Console.getInt("Enter number of times at bat: ", 1, 30);
+            Console.points();
 
-        System.out.print("Enter number of times at bat: ");
-        int bat = sc.nextInt();
-        int atBat = 0;
-        int hit = 0;
-        double average = 0;
-        double slugging = 0;
-        int sum = 0;
-        int[] number = new int[bat];
+            int atBat = 0;
+            int hit = 0;
+            int sum = 0;
 
-        for (int i = 0; i < number.length; i++) {
-            atBat++;
-            System.out.print("Result for at-bat " + atBat + ": ");
-            int result = sc.nextInt();
-            number[i] = result;
-        }
-       // Arrays.sort(number);
-        System.out.println(Arrays.toString(number));
+            int[] number = new int[times];
 
-        for (int i : number) {
-            sum += i;
-
-            if (i > 0) {
-                hit++;
+            for (int i = 0; i < number.length; i++) {
+                atBat++;
+                int result = Console.getInt("Result for at-bat " + atBat + ": ", -1, 5);
+                number[i] = result;
             }
+            System.out.println();
+
+            for (int i : number) {
+                sum += i;
+
+                if (i > 0) {
+                    hit++;
+                }
+            }
+            double slugging = (double) sum / number.length;
+            double average = (double) hit / number.length;
+
+            DecimalFormat formatPercentage = new DecimalFormat("#0.000");
+
+            System.out.println("Batting average: " + formatPercentage.format(average) + "\n");
+            System.out.println("Slugging percent: " + formatPercentage.format(slugging));
+
+            choice = Console.getString("Another player? (y/n): ", "y", "n");
+            System.out.println();
         }
-            slugging = (double) sum / number.length;
-            average = (double) hit / number.length;
-
-        System.out.println(slugging);
-        System.out.println(average);
-
+        System.out.println("Goodbye!");
     }
 
 }
---
+++ BattingStatistics/src/main/java/org/example/Console.java
@@ -0,0 +1,120 @@
+package org.example;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Console {
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void displayWelcome() {
+        System.out.println("Welcome to the Batting Average Calculator\n");
+    }
+
+    public static void points() {
+        System.out.println("\n0 = out, 1 = single, 2 = double, 3 = triple, 4 = home run");
+    }
+
+    public static int getRandomInt(int limit) {
+        double d = Math.random() * limit;
+        int i = (int) d;
+        i++;
+        return i;
+    }
+    public static String getString(String prompt) {
+        String s = "";
+        boolean isValid = false;
+        while (!isValid) {
+            System.out.print(prompt);
+            s = sc.nextLine();
+            if (s.isEmpty()) {
+                System.out.println("Error! This entry is required. Try again.\n");
+            } else {
+                isValid = true;
+            }
+        }
+        return s;
+    }
+
+    public static String getString(String prompt, String s1, String s2) {
+        String s = "";
+        boolean isValid = false;
+        while (!isValid) {
+            s = getString(prompt);
+            if (!s.equalsIgnoreCase(s1) && !s.equalsIgnoreCase(s2)) {
+                System.out.println("Error! Entry must be '" + s1 + "' or '" +
+                        s2 + "'. Try again.\n");
+            } else {
+                isValid = true;
+            }
+        }
+        return s;
+    }
+
+    public static int getInt(String prompt) {
+        int i = 0;
+        boolean isValid = false;
+        while (!isValid) {
+            System.out.print(prompt);
+            if (sc.hasNextInt()) {
+                i = sc.nextInt();
+                isValid = true;
+            } else {
+                System.out.println("Error! Invalid integer. Try again.\n");
+            }
+            sc.nextLine(); // discard any other data entered on the line
+        }
+        return i;
+    }
+
+    public static int getInt(String prompt, int min, int max) {
+        int i = 0;
+        boolean isValid = false;
+        while (!isValid) {
+            i = getInt(prompt);
+            if (i <= min) {
+                System.out.println(
+                        "Error! Number must be greater than " + min + ".\n");
+            } else if (i >= max) {
+                System.out.println(
+                        "Error! Number must be less than " + max + ".\n");
+            } else {
+                isValid = true;
+            }
+        }
+        return i;
+    }
+
+    public static double getDouble(String prompt) {
+        double d = 0;
+        boolean isValid = false;
+        while (!isValid) {
+            System.out.print(prompt);
+            if (sc.hasNextDouble()) {
+                d = sc.nextDouble();
+                isValid = true;
+            } else {
+                System.out.println("Error! Invalid number. Try again.\n");
+            }
+            sc.nextLine(); // discard any other data entered on the line
+        }
+        return d;
+    }
+
+    public static double getDouble(String prompt, double min, double max) {
+        double d = 0;
+        boolean isValid = false;
+        while (!isValid) {
+            d = getDouble(prompt);
+            if (d <= min) {
+                System.out.println(
+                        "Error! Number must be greater than " + min + ".\n");
+            } else if (d >= max) {
+                System.out.println(
+                        "Error! Number must be less than " + max + ".\n");
+            } else {
+                isValid = true;
+            }
+        }
+        return d;
+    }
+}
